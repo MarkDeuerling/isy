cbf546b5f1774751a38afcc7e881feecb9d29721
MarkDeuerling <deuerlingmark@yahoo.de>
2018-04-26 15:26:56 +0200

    2.3 done

--- 01_filter.py
+++ 01_filter.py
@@ -50,21 +50,37 @@ def convolution_2d(img, kernel):
     # Hint: you need the kernelsize
 
     offset = int(kernel.shape[0]/2)
-    newimg = np.zeros(img.shape)
+    output_img = np.zeros(img.shape)
 
-    # YOUR CODE HERE
+    image = cv2.copyMakeBorder(img, offset, offset, offset, offset, cv2.BORDER_REPLICATE)
+    rows, cols = img.shape[:2]
+    for y in np.arange(offset, rows + offset):
+        for x in np.arange(offset, cols + offset):
+            # extract the ROI of the image by extracting the
+            # *center* region of the current (x, y)-coordinates
+            # dimensions
+            roi = image[y - offset:y + offset + 1, x - offset:x + offset + 1]
 
-    return newimg
+            # perform the actual convolution by taking the
+            # element-wise multiplicate between the ROI and
+            # the kernel, then summing the matrix
+            k = (roi * kernel).sum()
+
+            # store the convolved value in the output (x,y)-
+            # coordinate of the output image
+            output_img[y - offset, x - offset] = k
+
+    return output_img
 
 
 if __name__ == "__main__":
 
     # 1. load image in grayscale
-    REAL_IMG_PATH = 'images/graycam.png'
+    REAL_IMG_PATH = 'images/Lenna.png'
     p = Path(__file__)
     IMG_PATH = str(p.absolute().parent.joinpath(REAL_IMG_PATH))
     # 2. convert image to 0-1 image (see im2double)
-    gray_img = cv2.imread(IMG_PATH)
+    gray_img = cv2.imread(IMG_PATH, 0)
     img_01 = im2double(gray_img)
 
     # image kernels
@@ -72,9 +88,17 @@ if __name__ == "__main__":
     sobelmask_y = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
     gk = make_gaussian(11)
 
+    blur_img = convolution_2d(img_01, gk)
+
     # 3 .use image kernels on normalized image
+    sobel_x = convolution_2d(blur_img, sobelmask_x)
+    sobel_y = convolution_2d(blur_img, sobelmask_y)
 
     # 4. compute magnitude of gradients
+    mag = np.sqrt(sobel_x ** 2 + sobel_y ** 2).astype(np.float)
+
+    normalized_img = np.zeros(mag.shape)
+    mog = cv2.normalize(mag, normalized_img, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
 
     # Show resulting images
     cv2.imshow("sobel_x", sobel_x)
