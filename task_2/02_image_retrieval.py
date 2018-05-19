f4954472c1a86d4f2ed6a15275100531415a156d
MarkDeuerling <deuerlingmark@yahoo.de>
2018-05-19 20:36:31 +0200

    task2 simple hog

--- task_2/02_image_retrieval.py
+++ task_2/02_image_retrieval.py
@@ -1,6 +1,7 @@
 import cv2
 import glob
-from Queue import PriorityQueue
+import numpy as np
+# from Queue import PriorityQueue
 
 ############################################################
 #
@@ -11,15 +12,17 @@ from Queue import PriorityQueue
 
 # implement distance function
 def distance(a, b):
-    # YOUR CODE HERE
-    pass
+    return cv2.norm(a, b, cv2.NORM_L2)
 
 
 def create_keypoints(w, h):
     keypoints = []
     keypointSize = 11
 
-    # YOUR CODE HERE
+    for x in range(0, w, keypointSize):
+        for y in range(0, h, keypointSize):
+            kp = cv2.KeyPoint(x, y, keypointSize)
+            keypoints.append(kp)
 
     return keypoints
 
@@ -33,16 +36,54 @@ keypoints = create_keypoints(256, 256)
 
 # 3. use the keypoints for each image and compute SIFT descriptors
 #    for each keypoint. this compute one descriptor for each image.
-
-# YOUR CODE HERE
+sift = cv2.xfeatures2d.SIFT_create()
+desc_img = []
+for img in images:
+    img = cv2.imread(img)
+    descriptor = sift.compute(img, keypoints)
+    # descriptors.append(descriptor)
+    desc_img.append((descriptor, img))
 
 # 4. use one of the query input image to query the 'image database' that
 #    now compress to a single area. Therefore extract the descriptor and
 #    compare the descriptor to each image in the database using the L2-norm
 #    and save the result into a priority queue (q = PriorityQueue())
 
-# YOUR CODE HERE
+query_img = 'query_face'
+query_img = glob.glob('./images/db/{}.jpg'.format(query_img))[0]
+query_img = cv2.imread(query_img)
+query_descriptor = sift.compute(query_img, keypoints)
 
+dists = []
+for d_i in desc_img:
+    desc = d_i[0]
+    d = distance(query_descriptor[1], desc[1])
+    dists.append((d, d_i[1]))
+
+# for descriptor in descriptors:
+#     d = distance(query_descriptor[1], descriptor[1])
+#     dists.append((d, descriptor))
+sorted_dists = sorted(dists, key=lambda x: x[0])
 # 5. output (save and/or display) the query results in the order of smallest distance
 
-# YOUR CODE HERE
+
+def query():
+    for q in sorted_dists:
+        yield q
+
+
+next_img = query()
+cur_img = next(next_img)
+while True:
+    ch = cv2.waitKey(1) & 0xFF
+    if ch == ord('q'):
+        break
+    if ch == ord('n'):
+        try:
+            cur_img = next(next_img)
+        except Exception:
+            print("No more Images.")
+    cv2.imshow('query img', query_img)
+    cv2.imshow('match img', cur_img[1])
+
+cv2.destroyAllWindows()
--- task_2/02_simple_hog.py
+++ task_2/02_simple_hog.py
@@ -4,6 +4,7 @@ import math
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+import glob
 
 ###############################################################
 #
@@ -22,25 +23,37 @@ def plot_histogram(hist, bins):
 def compute_simple_hog(imgcolor, keypoints):
 
     # convert color to gray image and extract feature in gray
+    gray = cv2.cvtColor(imgcolor, cv2.COLOR_BGR2GRAY)
 
     # compute x and y gradients
+    sobelx = cv2.Sobel(gray, cv2.CV_32F, 1, 0, ksize=3)
+    sobely = cv2.Sobel(gray, cv2.CV_32F, 0, 1, ksize=3)
 
     # compute magnitude and angle of the gradients
+    magnitude = cv2.magnitude(sobelx, sobely)
+    phase = cv2.phase(sobelx, sobely, True)
 
-    # go through all keypoints and and compute feature vector
+    # go through all keypoints and compute feature vector
     descr = np.zeros((len(keypoints), 8), np.float32)
     count = 0
     for kp in keypoints:
         # print kp.pt, kp.size
+        print('kp:{}, size:{}'.format(kp.pt, kp.size))
+        x = int(kp.pt[0])
+        y = int(kp.pt[1])
+        size = int(kp.size) >> 2
         # extract angle in keypoint sub window
+        phase_kp = phase[x-size:x+size, y-size:y+size]
+
         # extract gradient magnitude in keypoint subwindow
+        magnitude_kp = magnitude[x-size:x+size, y-size:y+size]
 
         # create histogram of angle in subwindow BUT only where magnitude of gradients is non zero! Why? Find an
         # answer to that question use np.histogram
-        #(hist, bins) = np.histogram(...)
+        non_zero_angle = phase_kp[magnitude_kp != 0]
 
+        (hist, bins) = np.histogram(non_zero_angle, bins=[0, 1, 2, 3, 4, 5, 6, 7, 8])
         plot_histogram(hist, bins)
-
         descr[count] = hist
 
     return descr
@@ -49,6 +62,6 @@ def compute_simple_hog(imgcolor, keypoints):
 keypoints = [cv2.KeyPoint(15, 15, 11)]
 
 # test for all test images
-test = cv2.imread('./images/hog_test/diag.jpg')
+test = cv2.imread('./images/hog_test/circle.jpg')
 descriptor = compute_simple_hog(test, keypoints)
 
